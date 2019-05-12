72ea43c2c2b4e97aea9d60df230b56cb79d31f3d
yermm <49121686+yermm@users.noreply.github.com>
2019-05-12 22:33:01 +0900

    ??

---
+++ src/Hanger.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics;
+
+public class Hanger {
+	private static volatile Hanger hanger = new Hanger();
+	public Graphics g;
+	
+	public Hanger() {
+		
+	}
+	
+	public static Hanger getHangerObject() {
+		return hanger;
+	}
+	
+	public void setGraphic(Graphics g) {
+		this.g = g;
+	}
+	
+	public void makeHanger() {
+		g.drawLine(50,550,375,550);
+	      g.drawLine(150,550,150,150);
+	      g.drawLine(150,150,375,150);
+	      g.drawLine(375,150,375,199);
+	}
+	
+}
---
+++ src/Interfaces.java
@@ -0,0 +1,9 @@
+
+interface PaintBody {
+
+    // reset the drawing to empty and clear the display
+    public boolean haveCount () ;
+
+    public Object next();
+
+}
